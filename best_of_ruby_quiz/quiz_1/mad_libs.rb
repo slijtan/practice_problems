04d7bf040bb0f42a2b6105007942ca4577f974ec
Lijen Tan <lijentan@gmail.com>
2013-06-06 15:15:24 -0700

    refactoring into 3 classes

--- best_of_ruby_quiz/quiz_1/mad_libs.rb
+++ best_of_ruby_quiz/quiz_1/mad_libs.rb
@@ -1,12 +1,16 @@
 module MadLib
   class Storyteller
-    def initialize(story_template)
-      @story_template = story_template
+    def initialize(story_template, input = STDIN, output = STDOUT)
+      @story = Story.new(story_template, input, output)
     end
 
-    def tell_story(input = STDIN, output = STDOUT)
-      story = @story_template
-      stored_words = {}
+    def tell_story
+      @story.word_spaces do |word_space|
+        word_replacement = @story.get_word_replacement(word_space)
+        @story.place_word(word_space, word_replacement)
+      end
+
+      return @story.story
 
       while match_data = story.match(/(\(\((.+?)\)\))/)
         word_space = match_data[2]
@@ -18,8 +22,8 @@ module MadLib
         end
 
         unless(user_word = stored_words[word_space])
-          user_word = input.gets
           output.puts "Please provide #{word_space}"
+          user_word = input.gets
         end
 
         story[start...finish] = user_word
@@ -36,6 +40,63 @@ module MadLib
       if (match = word.match(/(.+):(.+)/))
         return [match[1], match[2]]
       end
+    end
+  end
+
+  class Story
+    attr_reader :story
+
+    def initialize(story_template, input, output)
+      @story_template = story_template
+      @story = story_template
+      @input = input
+      @output = output
+      @stored_words = {}
+    end
+
+    def word_spaces(&block)
+      while match_data = @story.match(/(\(\((.+?)\)\))/)
+        word_name = match_data[2]
+        start, finish = match_data.offset(1)
+        yield WordSpace.new(word_name, start, finish)
+      end
+    end
+
+    def get_word_replacement(word_space)
+      if @stored_words[word_space.name]
+        word_replacement = @stored_words[word_space.name]
+      else
+        @output.puts "Please provide #{word_space.name}"
+        word_replacement = @input.gets
+
+        @stored_words[word_space.var_name] = word_replacement if word_space.is_set_repeating?
+      end
+
+      word_replacement
+    end
+
+    def place_word(word_space, word_replacement)
+      @story[word_space.start...word_space.finish] = word_replacement
+    end
+  end
+
+  class WordSpace
+    attr_reader :name, :start, :finish, :var_name
+
+    def initialize(name, start, finish)
+      if (match = name.match(/(.+):(.+)/))
+        @var_name = match[1]
+        @name = match[2]
+      else
+        @name = name
+      end
+
+      @start = start
+      @finish = finish
+    end
+
+    def is_set_repeating?
+      !!@var_name
     end
   end
 
--- best_of_ruby_quiz/quiz_1/mad_libs_spec.rb
+++ best_of_ruby_quiz/quiz_1/mad_libs_spec.rb
@@ -1,33 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) << "/mad_libs.rb")
 
 describe MadLib::Storyteller, "#tell_story" do
-  before  do
-    word_num = 0
-    @stdin = double("stdin")
-    @stdin.stub(:gets) { word_num += 1; "word#{word_num}" }
-    @stdout = double("stdout")
-  end
-
   context "without reused phrases" do
-    before(:all) { @mad_lib = MadLib::Storyteller.new('I had a ((an adjective)) sandwich for lunch today. It dripped all over my ((a body part)) and ((a noun)).') }
+    before do
+      word_num = 0
+      @stdin = double("stdin")
+      @stdin.stub(:gets) { word_num += 1; "word#{word_num}" }
+      @stdout = double("stdout")
+
+      @mad_lib = MadLib::Storyteller.new('I had a ((an adjective)) sandwich for lunch today. It dripped all over my ((a body part)) and ((a noun)).', @stdin, @stdout)
+    end
 
     it "prompts the user for input on phrases formatted by ((<phrase>)) and replaces it in the string" do
       @stdout.should_receive(:puts).with("Please provide an adjective")
       @stdout.should_receive(:puts).with("Please provide a body part")
       @stdout.should_receive(:puts).with("Please provide a noun")
 
-      @mad_lib.tell_story(@stdin, @stdout).should == 'I had a word1 sandwich for lunch today. It dripped all over my word2 and word3.'
+      @mad_lib.tell_story.should == 'I had a word1 sandwich for lunch today. It dripped all over my word2 and word3.'
     end
   end
 
   context "with reused phrases" do
-    before(:all) { @mad_lib = MadLib::Storyteller.new('Our favorite language is ((gem:a gemstone)). We think ((gem)) is better than ((a gemstone)).') }
+    before do
+      word_num = 0
+      @stdin = double("stdin")
+      @stdin.stub(:gets) { word_num += 1; "word#{word_num}" }
+      @stdout = double("stdout")
+      @mad_lib = MadLib::Storyteller.new('Our favorite language is ((gem:a gemstone)). We think ((gem)) is better than ((a gemstone)).', @stdin, @stdout)
+    end
 
     it "prompts the user for input on phrases formatted by ((<var>:<phrase>)) and replaces future occurences of ((<var>)) found in string" do
       @stdout.should_receive(:puts).with("Please provide a gemstone")
       @stdout.should_receive(:puts).with("Please provide a gemstone")
 
-      @mad_lib.tell_story(@stdin, @stdout).should == 'Our favorite language is word1. We think word1 is better than word2.'
+      @mad_lib.tell_story.should == 'Our favorite language is word1. We think word1 is better than word2.'
     end
   end
 end
