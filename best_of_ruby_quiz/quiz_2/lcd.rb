178ae75bad7c2f6a882a104fad8165b83e8df846
Lijen Tan <lijentan@gmail.com>
2013-06-06 17:49:06 -0700

    working version, a bit manual, but whatever, it gets the job done for now

--- best_of_ruby_quiz/quiz_2/lcd.rb
+++ best_of_ruby_quiz/quiz_2/lcd.rb
@@ -1,9 +1,81 @@
-class LCD
-  def initialize(number)
-    @number = number
+module LCD
+  class Screen
+    def initialize(number)
+      @digits = []
+      number.each_char {|digit| @digits << LCD::Digit.new(digit)}
+    end
+
+    def print_display(size = 2)
+      res = []
+      row = 0
+
+      #row 0
+      @digits.each do |digit|
+        str = ""
+        str << " "
+        if digit.display_position?(0)
+          str << "_" * size
+        else
+          str << " " * size
+        end
+        str << " "
+
+        res[row] ||= []
+        res[row] << str
+      end
+      row += 1
+
+      #row 1
+      size.downto(1) do |i|
+        @digits.each do |digit|
+          str = ""
+          str << (digit.display_position?(1) ? "|" : " ")
+          str << (i == 1 && digit.display_position?(3) ? "_" : " ") * size #middle horizontal line
+          str << (digit.display_position?(2) ? "|" : " ")
+
+          res[row] ||= []
+          res[row] << str
+        end
+
+        row += 1
+      end
+
+      #row 2
+      size.downto(1) do |i|
+        @digits.each do |digit|
+          str = ""
+          str << (digit.display_position?(4) ? "|" : " ")
+          str << (i == 1 && digit.display_position?(6) ? "_" : " ") * size #bottom horizontal line
+          str << (digit.display_position?(5) ? "|" : " ")
+
+          res[row] ||= []
+          res[row] << str
+        end
+
+        row += 1
+      end
+
+      res.map { |row| row.join(" ") }.join("\n")
+    end
   end
 
-  def print_display(size = 2)
+  class Digit
+    FILLMAP = [
+               "1110111".to_i(2),
+               "0100100".to_i(2),
+               "1011101".to_i(2),
+               "1101101".to_i(2)
+              ]
+
+    attr_reader :digit
+
+    def initialize(digit)
+      @digit = digit.to_i
+    end
+
+    def display_position?(position)
+      (FILLMAP[@digit] & (1 << position)) > 0
+    end
 
   end
 end
--- best_of_ruby_quiz/quiz_2/lcd_spec.rb
+++ best_of_ruby_quiz/quiz_2/lcd_spec.rb
@@ -1,23 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) << '/lcd.rb')
 
-describe LCD, "#print" do
-  before { @lcd = LCD.new(0123) }
+describe LCD do
+  describe LCD::Screen, "#print" do
+    before { @lcd = LCD::Screen.new("0123") }
 
-  it "prints out the number at size 1" do
-    @lcd.print_display(1).should == <<eos
- _        _   _
-| |    |  _|  _|
-|_|    | |_   _|
-eos
+    it "prints out the number at size 1" do
+      result =  " _       _   _ \n"
+      result << "| |   |  _|  _|\n"
+      result << "|_|   | |_   _|"
+
+      @lcd.print_display(1).should == result
+    end
+
+    it "prints out the number at size 2" do
+      result =  " __        __   __ \n"
+      result << "|  |    |    |    |\n"
+      result << "|  |    |  __|  __|\n"
+      result << "|  |    | |       |\n"
+      result << "|__|    | |__   __|"
+
+      @lcd.print_display.should == result
+    end
   end
 
-  it "prints out the number at size 2" do
-    @lcd.print_display.should == <<eos
- __        __   __
-|  |    |    |    |
-|  |    |  __|  __|
-|  |    | |       |
-|__|    | |__   __|
-eos
+  describe LCD::Digit do
+    it "tells us if we should display a line posiition" do
+      digit = LCD::Digit.new(0)
+      digit.display_position?(0).should be_true
+      digit.display_position?(1).should be_true
+      digit.display_position?(2).should be_true
+      digit.display_position?(3).should be_false
+      digit.display_position?(4).should be_true
+      digit.display_position?(5).should be_true
+      digit.display_position?(6).should be_true
+    end
   end
+
+
 end
