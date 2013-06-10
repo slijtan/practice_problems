96b4468b92993620f40dfa61066a9e5d3c08227b
Lijen Tan <lijentan@gmail.com>
2013-06-10 12:45:01 -0700

    fixing tests

--- best_of_ruby_quiz/quiz_5/scrabble_stems_rspec.rb
+++ best_of_ruby_quiz/quiz_5/scrabble_stems_rspec.rb
@@ -6,21 +6,21 @@ describe Dictionary, "#scrabble_stems" do
   end
 
   it "finds all scrabble stems with 1 cutoff" do
-    @dictionary.scrabble_stems(1).should include("aeirst")
-    @dictionary.scrabble_stems(1).should include("aairst")
-    @dictionary.scrabble_stems(1).should include("aaerst")
-    @dictionary.scrabble_stems(1).should include("aaeist")
-    @dictionary.scrabble_stems(1).should include("aaeirt")
-    @dictionary.scrabble_stems(1).should include("aaeirs")
+    stems = @dictionary.scrabble_stems(1)
+    stems.size.should == 18
+    stems.should include("aeirst")
+    stems.should include("aairst")
+    stems.should include("aaerst")
+    stems.should include("aaeist")
+    stems.should include("aaeirt")
+    stems.should include("aaeirs")
+    stems.should include("aaeirs")
   end
 
   it "finds all scrabble stems with 3 cutoff" do
-    @dictionary.scrabble_stems(1).should include("aeirst")
-    @dictionary.scrabble_stems(1).should include("aairst")
-    @dictionary.scrabble_stems(1).should include("aaerst")
-    @dictionary.scrabble_stems(1).should include("aaeist")
-    @dictionary.scrabble_stems(1).should include("aaeirt")
-    @dictionary.scrabble_stems(1).should include("aaeirs")
+    stems = @dictionary.scrabble_stems(3)
+    stems.size.should == 1
+    stems.should == ["aeirst"]
   end
 
   it "finds all scrabble stems with 4 cutoff" do
