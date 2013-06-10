fb7346ef54177f204dbf75fc1da857047163fcfe
Lijen Tan <lijentan@gmail.com>
2013-06-10 15:32:31 -0700

    adding quick 0/1 knapsack solution

---
+++ random_questions/knapsack.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+#google for 0/1 knapsack problem to find problem
+
+class KnapObj
+  attr_reader :w, :v
+
+  def initialize(w, v)
+    @w = w
+    @v = v
+  end
+end
+
+def maximize(set, weight)
+  return 0 if set.empty?
+  item = set.shift
+
+  if(item.w <= weight)
+    with_self = item.v + maximize(set.clone, weight - item.w)
+    without_self = maximize(set.clone, weight)
+
+    return [with_self, without_self].max
+  else
+    return maximize(set, weight)
+  end
+end
+
+a = KnapObj.new(3,5)
+b = KnapObj.new(2,3)
+c = KnapObj.new(1,4)
+
+p maximize([b,c], 2)
+p maximize([a,b,c], 5)
