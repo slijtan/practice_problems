77644be11c3faacd7ae8b64a3a154c7f74ec5300
Lijen Tan <lijentan@gmail.com>
2013-06-10 21:14:01 -0700

    updating syntax

--- random_questions/my_attr_accessor.rb
+++ random_questions/my_attr_accessor.rb
@@ -4,7 +4,7 @@ class Object
       define_method(name) { instance_variable_get("@#{name}") }
       define_method("#{name}=") {|arg| instance_variable_set("@#{name}",arg)}
 
-      class << self ; self end.class_eval do
+      class << self ; self ; end.class_eval do
         define_method(name) {"UNIVERSALLY HUGE #{name}"}
       end
     end
