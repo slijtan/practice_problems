3c626d3195e12563a01fb1f716bd30e9db3455e4
Lijen Tan <lijentan@gmail.com>
2013-06-12 15:24:24 -0700

    working run script as well as generated solutions

--- google_code_jam/minimum_scalar_product/run.rb
+++ google_code_jam/minimum_scalar_product/run.rb
@@ -3,15 +3,20 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'minimum_scalar_product.rb'))
 
 input = ARGV[0]
+output = ARGV[1]
 
-File.open(input) do |f|
-  test_cases = f.gets.chomp.to_i
+File.open(output, 'w') do |fout|
 
-  test_cases.times do |i|
-    array_size = f.gets.chomp.to_i
-    a = f.gets.chomp.split(" ").map(&:to_i)
-    b = f.gets.chomp.split(" ").map(&:to_i)
-    sum = a.min_scalar_product(b)
-    p "Case ##{i+1} #{sum}"
+  File.open(input) do |f|
+    test_cases = f.gets.chomp.to_i
+
+    test_cases.times do |i|
+      array_size = f.gets.chomp.to_i
+      a = f.gets.chomp.split(" ").map(&:to_i)
+      b = f.gets.chomp.split(" ").map(&:to_i)
+      sum = a.min_scalar_product(b)
+      fout.puts "Case ##{i+1}: #{sum}"
+    end
   end
+
 end
